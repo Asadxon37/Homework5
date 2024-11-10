63add88cb89ce6c43e6405c3a2d6f3caff39372c
Asadxon37 <m.usmonov@newuu.uz>
2024-11-10 23:21:17 +0500

    Git init

--- problem4.cpp
+++ problem4.cpp
@@ -1,42 +1,34 @@
 #include <iostream>
-#include <climits>
-
+#include <cmath>
 using namespace std;
 
-int maxProfit(int prices[], int n) {
-    int min_price = INT_MAX;
-    int max_profit = 0;
-    int min_day = -1;
-    int buy_day = 0;
-    int sell_day = 0;
-
-    for (int i = 0; i < n; ++i) {
-        if (prices[i] < min_price) {
-            min_price = prices[i];
-            min_day = i;
-        } else {
-            int profit = prices[i] - min_price;
-            if (profit > max_profit) {
-                max_profit = profit;
-                buy_day = min_day;
-                sell_day = i+1;
-            }
+int sort(int arr[],int n) {
+    int profit=INT_MAX;
+    int sellDay=0;
+    int buyDay=0;
+    int max=0;
+    for(int i=0;i<n;i++) {
+        if(arr[i]<profit) {
+            profit=arr[i];
+            buyDay=i;
+        }
+        int profit_now=arr[i]-profit;
+        if(profit_now>max) {
+            max=profit_now;
+            sellDay=i;
         }
     }
-    if (max_profit == 0) {
-        return 0;
-    }
-    return sell_day;
+    if (max>0) {
+        return sellDay+1;
+    }else{return 0;}
 }
+
 int main() {
     int n;
-    cin >> n;
+    cin>>n;
     int prices[n];
-    for (int i = 0; i < n; ++i) {
-        cin >> prices[i];
+    for(int i=0;i<n;i++) {
+        cin>>prices[i];
     }
-    int max_profit = maxProfit(prices, n);
-    if (max_profit > 0) {
-        cout << max_profit << endl;
-    }if(max_profit == 0) {cout << 0 << endl;}
+    cout<<sort(prices,n);
 }
